41d7077de0bd98130f7361313ecd280309ac07f6
sakshi16-b <sakshibisht016@gmail.com>
2025-06-24 11:15:49 +0530

    changes in contact-country-countrycard

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,7 @@
 export const Contact = () => {
-    const handleFormSubmit=(formData)=>{
-        console.log(formData.entries())
-        const formInputData=Object.fromEntries(formData.entries())
+    const handleFormSubmit = (formData) => {
+        //console.log(formData.entries())
+        const formInputData = Object.fromEntries(formData.entries())
         console.log(formInputData);
 
     }
@@ -9,29 +9,34 @@ export const Contact = () => {
     return (
         <section className="section-contact">
             <h2 className="container-title">Contact Us</h2>
-
-            <div className="container-wrapper">
+            <div className="contact-wrapper container">
                 <form action={handleFormSubmit}>
                     <input type="text"
                         className="form-control"
-                        required
-                        autoComplete="false"
                         placeholder="Enter Your Name"
-                        name="username" />
-
-                    <input type="text"
-                        className="form-control"
+                        name="username"
                         required
-                        autoComplete="false"
+                        autoComplete="off"
+                    />
+
+                    <input type="email"
+                        className="form-control"
                         placeholder="Enter Your Email"
-                        name="email" />
+
+                        name="email"
+                        required
+                        autoComplete="off"
+                    />
 
                     <textarea
                         className="form-control"
                         rows="10"
-                        autoComplete="false"
+                        name="message"
                         placeholder="Enter Your Message"
-                        name="message" />
+                        required
+                        autoComplete="off"></textarea>
+
+
 
                     <button type="submit" value="send">Send</button>
 
